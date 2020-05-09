a05c3d38dcf8fb2969b8917cb734395f933b2490
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2020-05-09 14:32:48 -0500

    Added initial routes and component files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/navBar";
+import Tasks from "./components/tasks";
+import NotFound from "./components/notFound";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route path="/tasks" component={Tasks} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/tasks" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </React.Fragment>
   );
 }
 
