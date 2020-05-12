b21e1f906eb6fabff0ebc7f9d9e18528e0968986
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2020-05-11 21:07:07 -0500

    Added elements to navigation bar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        App
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/tasks">
+            Tareas
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/record">
+            Historial
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
